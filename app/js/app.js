ad770de15c9a64a16a2fbbffd217e07d06fb1df0
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-05-03 15:25:22 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -15,15 +15,9 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
   const createDiamond = (size) => {
     const container = document.getElementById(`diamond-container`);
-    container.innerHTML = ``; // Clear previous diamond
-
+    container.innerHTML = ``;
     const diamondWrapper = document.createElement(`div`);
     diamondWrapper.classList.add(`diamond-wrapper`);
-
-    // If even, make it odd
-    if (size % 2 === 0) {
-        size++;
-    }
 
     for (let i = 0; i < size; i++) {
         const row = document.createElement(`div`);
