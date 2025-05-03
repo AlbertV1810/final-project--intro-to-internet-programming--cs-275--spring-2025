95c82afb29b4536d47da23c0c0eeca10b62dd6de
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-05-03 15:07:04 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -27,7 +27,7 @@ document.addEventListener(`DOMContentLoaded`, () => {
         row.classList.add(`diamond-row`);
 
         const spaces = Math.abs(Math.floor(size / 2) - i);
-        const stars = size - spaces * 2;
+        const stars = 2 * i + 1 - Math.abs(size - (2 * i + 1));
         row.textContent = `${` `.repeat(spaces)}${`*`.repeat(stars)}`;
         diamondWrapper.appendChild(row);
     }
