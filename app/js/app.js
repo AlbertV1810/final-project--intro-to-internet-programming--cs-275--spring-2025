722c5b8c7fed208c755b11d839f5286528abbafe
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-05-04 19:38:38 -0400

    removed even case for now

--- app/js/app.js
+++ app/js/app.js
@@ -24,18 +24,13 @@ document.addEventListener(`DOMContentLoaded`, () => {
     diamondWrapper.classList.add(`diamond-wrapper`);
 
     if (isEven) {
-        for (let i = 0; i < size; i++) {
+        for(let i = 0; i < size; i++) {
             const row = document.createElement(`div`);
             row.classList.add(`diamond-row`);
 
-            let spaces = Math.abs(size / 2 - i);
-            let stars = 2 * Math.abs(size / 2 - i) + 1;
-
-            if (i === 0 || i === size - 1) {
-                stars = 1;
-            }
-
-            row.textContent = `${" ".repeat(spaces)}${"*".repeat(stars)}`;
+            let spaces = Math.abs(Math.floor(size / 2) - i);
+            let stars = size - spaces * 2;
+            row.textContent = `${` `.repeat(spaces + 1)}${`*`.repeat(stars)}`;
             diamondWrapper.appendChild(row);
         }
 
